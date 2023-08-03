0ce2a2ed2c76df211f95eaf50859f62910b8794b
heosumin518 <heosumin518@gmail.com>
2023-08-03 15:24:27 +0900

    Delete Debug folders and Setup Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Hazel applications
+
+#include "Hazel/Application.h";		// 또는 "Hazel/Core.h" 등등 여러가지를 추가한다.
+
+// --Entry Point-----------------------
+#include "Hazel/EntryPoint.h"
+// ------------------------------------
