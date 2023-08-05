580666f00af8bfa5407334bd836f48206a95d083
heosumin <heosumin518@gmail.com>
2023-08-05 12:07:44 +0900

    Make Logging System

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 // For use by Hazel applications
 
 #include "Hazel/Application.h";		// 또는 "Hazel/Core.h" 등등 여러가지를 추가한다.
+#include "Hazel/Log.h";
 
 // --Entry Point-----------------------
 #include "Hazel/EntryPoint.h"
